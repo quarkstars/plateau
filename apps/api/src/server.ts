89e7c6759b4cd355ce5873fe319344ac6d764399
quarkstars <michael.h@quarkstar.co>
2023-11-05 07:05:56 -0800

    refactor: use express urlencoded and json

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,22 +1,41 @@
-import { json, urlencoded } from "body-parser";
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 
 export const createServer = () => {
-  const app = express();
-  app
-    .disable("x-powered-by")
-    .use(morgan("dev"))
-    .use(urlencoded({ extended: true }))
-    .use(json())
-    .use(cors())
-    .get("/message/:name", (req, res) => {
-      return res.json({ message: `hello ${req.params.name}` });
-    })
-    .get("/healthz", (req, res) => {
-      return res.json({ ok: true });
-    });
+	const app = express();
 
-  return app;
+	/**
+	 *  Boilerplate middleware
+	 */
+	// Logging
+	const morganMode = process.env.NODE_ENV === "production" ? "combined" : "dev";
+	app.use(morgan(morganMode));
+	// Body parsing
+	app.use(express.urlencoded({ extended: true }));
+	app.use(express.json());
+	// Cors
+	const corsOrigins =
+		process.env.NODE_ENV === "production"
+			? {
+					origin: process.env.ALLOWED_ORIGIN,
+			  }
+			: undefined;
+	app.use(cors(corsOrigins));
+	// Hide powered by express (security)
+	app.disable("x-powered-by");
+	// Hello world (name)
+	app.get("/message/:name", (req, res) => {
+		return res.json({ message: `hello ${req.params.name}` });
+	});
+	// Health check
+	app.get("/healthz", (req, res) => {
+		return res.json({ ok: true });
+	});
+
+	/**
+	 *  Boilerplate middleware
+	 */
+
+	return app;
 };
